0eb43607360960de51bb5f2071ccf83dc3430bf4
LeVar Gilford <lgilford@gmail.com>
2016-08-03 21:42:58 -0400

    ctual files

---
+++ SuperBets/ViewController.swift
@@ -0,0 +1,120 @@
+//
+//  ViewController.swift
+//  SuperBets
+//
+//  Created by LeVar Gilford on 8/3/16.
+//  Copyright © 2016 LeVar Gilford. All rights reserved.
+//
+import UIKit
+
+class ViewController: UIViewController, UIPageViewControllerDataSource {
+    
+    // Variables
+    private var pageViewController: UIPageViewController?
+    
+    // Initialize it right away here
+    private let contentImages = ["Main",
+                                 "A",
+                                 "B",
+                                 "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+    
+    //  View Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        createPageViewController()
+        setupPageControl()
+    }
+    
+    private func createPageViewController() {
+        
+        let pageController = self.storyboard!.instantiateViewControllerWithIdentifier("PageController") as! UIPageViewController
+        pageController.dataSource = self
+        
+        if contentImages.count > 0 {
+            let firstController = getItemController(0)!
+            let startingViewControllers = [firstController]
+            pageController.setViewControllers(startingViewControllers, direction: UIPageViewControllerNavigationDirection.Forward, animated: false, completion: nil)
+        }
+        
+        pageViewController = pageController
+        addChildViewController(pageViewController!)
+        self.view.addSubview(pageViewController!.view)
+        pageViewController!.didMoveToParentViewController(self)
+    }
+    
+    private func setupPageControl() {
+        let appearance = UIPageControl.appearance()
+        appearance.pageIndicatorTintColor = UIColor.grayColor()
+        appearance.currentPageIndicatorTintColor = UIColor.whiteColor()
+        appearance.backgroundColor = UIColor.darkGrayColor()
+    }
+    
+    // UIPageViewControllerDataSource
+    
+    func pageViewController(pageViewController: UIPageViewController, viewControllerBeforeViewController viewController: UIViewController) -> UIViewController? {
+        
+        let itemController = viewController as! PageItemController
+        
+        if itemController.itemIndex > 0 {
+            return getItemController(itemController.itemIndex-1)
+        }
+        
+        return nil
+    }
+    
+    func pageViewController(pageViewController: UIPageViewController, viewControllerAfterViewController viewController: UIViewController) -> UIViewController? {
+        
+        let itemController = viewController as! PageItemController
+        
+        if itemController.itemIndex+1 < contentImages.count {
+            return getItemController(itemController.itemIndex+1)
+        }
+        
+        return nil
+    }
+    
+    private func getItemController(itemIndex: Int) -> PageItemController? {
+        
+        if itemIndex < contentImages.count {
+            let pageItemController = self.storyboard!.instantiateViewControllerWithIdentifier("ItemController") as! PageItemController
+            pageItemController.itemIndex = itemIndex
+            pageItemController.imageName = contentImages[itemIndex]
+            return pageItemController
+        }
+        
+        return nil
+    }
+    
+    // MARK: - Page Indicator
+    
+    func presentationCountForPageViewController(pageViewController: UIPageViewController) -> Int {
+        return contentImages.count
+    }
+    
+    func presentationIndexForPageViewController(pageViewController: UIPageViewController) -> Int {
+        return 0
+    }
+    
+    // MARK: - Additions
+    
+    func currentControllerIndex() -> Int {
+        
+        let pageItemController = self.currentController()
+        
+        if let controller = pageItemController as? PageItemController {
+            return controller.itemIndex
+        }
+        
+        return -1
+    }
+    
+    func currentController() -> UIViewController? {
+        
+        if self.pageViewController?.viewControllers?.count > 0 {
+            return self.pageViewController?.viewControllers![0]
+        }
+        
+        return nil
+    }
+    
+}
